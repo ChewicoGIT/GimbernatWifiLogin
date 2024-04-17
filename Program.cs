61ee2aee36a018d5f24832e86e7e6fe64ca1c35d
Chewico <chewico3d@gmail.com>
2024-04-17 10:46:22 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Text;
+
+namespace GimbernatWifLogIn {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Thread.Sleep(1000);
+
+            WebRequest wr = WebRequest.Create(
+                "https://wifi.eug.es/login.html");
+
+
+            byte[] bytes = Encoding.UTF8.GetBytes("buttonClicked=4&" +
+                "redirect_url=" + Uri.EscapeDataString("www.msfconnecttest.com/redirect")
+                + "&err_flag=0" +
+                "&info_flag=0" +
+                "&info_msg=0" +
+                "&network_name=" + Uri.EscapeDataString("Guest Network"));
+
+            wr.ContentType = "application/x-www-form-urlencoded";
+            wr.ContentLength = bytes.Length;
+            wr.Method = "POST";
+
+            Stream inDataS = wr.GetRequestStream();
+            inDataS.Write(bytes, 0, bytes.Length);
+            inDataS.Close();
+
+            HttpWebResponse response = (HttpWebResponse)wr.GetResponse();
+
+            Console.WriteLine(response.StatusDescription);
+            response.Close();
+        }
+    }
+}
